81528b46e0cba7ed60f3f3b0c02e2093ef3b96fa
Tereza Hercíková <tereza.hercikova@gmail.com>
2021-04-03 11:06:09 +0200

    Umístění panáčka na střed

--- main.js
+++ main.js
@@ -1,78 +1,83 @@
-// toto budeš potřebovat později
-/*
-if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
-	// panacek a mince se prekryvaji
+// nadefinujeme globální proměnné
+// ty jsou pak použitelné kdekoliv v programu
+let panacek, panacekX, panacekY, panacekSirka, panacekVyska;
+let mince, minceX, minceY, minceSirka, minceVyska;
+
+panacek = document.querySelector("#panacek");
+mince = document.querySelector("#mince");
+
+// tato funkce se spustí při načtení stránky
+// tj. ve chvíli, kdy je načtené komplet HTML, CSS a všechny obrázky a zvuky
+function priNacteniStranky() {
+
+	// do globálních proměnných si uložíme odkaz na objekty panáčka a mince,
+	// abychom je nemuseli při každém použití znovu na stránce hledat pomocí document.querySelector
+
+
+	// zjistíme šířku a výšku panáčka
+	panacekSirka = panacek.width;
+	panacekVyska = panacek.height;
+
+	// a umístíme panáčka do středu okna
+	panacekX = Math.round(window.innerWidth / 2 - panacekSirka / 2);
+	panacekY = Math.round(window.innerHeight / 2  - panacekVyska / 2);
+
+	// umístíme panáčka na startovní pozici
+	umistiPanacka();
+
+
+	// zjistíme šířku a výšku mince
+	minceSirka = mince.width;
+	minceVyska = mince.height;
+
+
+	// a vygenerujeme první minci na náhodné pozici
+	novaMince();
+
 }
-*/
 
-
-// sem začni psát svůj program
-
-//1. POHYB PANÁČKA PO PLOŠE
-//definuju si proměnnou panáčka a o kolik px chci, aby se mi hýbal
-let panacek = document.querySelector('#panacek');
-let moveBy = 10; 
-
-window.addEventListener('load', () =>{
-	// startovní pozice panáčka
-	panacek.style.left = '100px';
-	panacek.style.top = '100px';
-
-
-});
-
-//definuju si pohyb šipek a o kolik se posouvají
-window.addEventListener('keyup', (e) =>{
-	switch(e.key){
-		case 'ArrowLeft':
-			panacek.style.left = parseInt(panacek.style.left) - moveBy + 'px';
-			break;
-		case 'ArrowRight':
-			panacek.style.left = parseInt(panacek.style.left) + moveBy + 'px';
-			break;
-		case 'ArrowUp':
-			panacek.style.top = parseInt(panacek.style.top) - moveBy + 'px';
-			break;
-		case 'ArrowDown':
-			panacek.style.top = parseInt(panacek.style.top) + moveBy + 'px';
-			break;
-			
-	};
-
-});
-
-
-//2. POHYB MINCE PO PLOŠE
-
-function getRandomPosition(element) {
-	var x = document.body.offsetHeight-element.clientHeight;
-	var y = document.body.offsetWidth-element.clientWidth;
-	var randomX = Math.floor(Math.random()*x);
-	var randomY = Math.floor(Math.random()*y);
-	return [randomX,randomY];
+// funkce, která umístí panáčka na jeho souřadnice
+// tj. nastaví jeho style.left a style.top na hodnoty panacekX, panacekY
+function umistiPanacka() {
+	// musíme to napsat :)
+	panacek.style.left = panacekX + 'px';
+	panacek.style.top = panacekY + 'px';
+	console.log("zkoušíš umístit panáčka");
 }
-window.onload = function() {
-	let img = document.getElementById("mince");
-	document.body.appendChild(img);
-	var xy = getRandomPosition(img);
-	img.style.top = xy[0] + 'px';
-	img.style.left = xy[1] + 'px';
+
+// funkce pro nahodné vygenerování nové pozice mince
+// a umístění mince na tyto souřadnice
+function novaMince() {
+	// musíme to napsat :)
 }
-    
 
-    
+// tato funkce se zavolá při stisku klávesy
+// do proměnné "udalost" se vloží objekt s parametry události¨
+// kde lze najít např. i vlastnost "key",
+// která obsahuje znak stisknuté klávesy
+function priStiskuKlavesy(udalost) {
 
-//3. KONTROLA PROTÍNÁNÍ MINCE A PANÁČKA
-
-// if (!( panacekX + panacekSirka < minceX || 
-// 	minceX + minceSirka < panacekX || 
-// 	panacekY + panacekVyska < minceY || 
-// 	minceY + minceVyska < panacekY)) {
-// 	// panacek a mince se prekryvaji
-// }
+	// šipka vlevo
 
 
+	// šipka vpravo
 
 
+	// šipka nahoru
 
 
+	// šipka dolů
+
+
+	// panáčka umistíme na nově vypočítanou pozici
+
+
+	// otestujeme kolizi panáčka s mincí
+
+
+}
+
+// fuknce pro otestování kolize panáčka s mincí
+function otestujKolizi() {
+	// musíme to napsat :)
+}
